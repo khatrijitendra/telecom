2b014d8f4d695f14ce2c938c9112ef80d1b5d9b3
Makarand <mbauskar@gmail.com>
2015-05-20 20:11:04 +0530

    [Telecom-Advocate] telecom app first commit

---
+++ telecom/hooks.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "telecom"
+app_title = "telecom"
+app_publisher = "telecom"
+app_description = "telecom"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "makarand.b@indictranstech.com"
+app_version = "0.0.1"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/telecom/css/telecom.css"
+# app_include_js = "/assets/telecom/js/telecom.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/telecom/css/telecom.css"
+# web_include_js = "/assets/telecom/js/telecom.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "telecom.install.before_install"
+# after_install = "telecom.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "telecom.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"telecom.tasks.all"
+# 	],
+# 	"daily": [
+# 		"telecom.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"telecom.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"telecom.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"telecom.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "telecom.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "telecom.event.get_events"
+# }
+
